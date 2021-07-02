b5619db34b0268619aa2889e79c5528ac11145f2
Saliq <saliq.ilshat@icloud.com>
2021-07-02 20:04:28 +0500

    the application is ready

--- RealmDB/TasksTableViewController.swift
+++ RealmDB/TasksTableViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TasksTableViewController: UITableViewController {
-
-    var tasksArray = ["Хлеб", "Молоко","Яйца", "Масло", "Макороны"]
+    
+    let realm = try! Realm()
+    
+    var tasksArray: Results<TaskStruct>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tasksArray = realm.objects(TaskStruct.self)
     }
 
     @IBAction func addTaskButton(_ sender: Any) {
@@ -31,14 +34,16 @@ class TasksTableViewController: UITableViewController {
         let saveAction = UIAlertAction(title: "Сохранить", style: .default) { action in
             
             // Проверяем не является ли текстовое поле пустым
-            guard alertTextField.text?.isEmpty == false else {
-                print("The text field is empty") // Выводим сообщение на консоль, если поле не заполнено
-                return
-            }
+            guard let text = alertTextField.text, alertTextField.text?.isEmpty == false else {return}
             
             // Добавляем в массив новую задачу из текстового поля
-            self.tasksArray.append((alert.textFields?.first?.text)!)
+//            self.tasksArray.append((alert.textFields?.first?.text)!)
             
+            let task = TaskStruct()
+            task.task = text
+            try! self.realm.write {
+                self.realm.add(task)
+            }
             // Обновляем таблицу
             self.tableView.reloadData()
         }
@@ -66,18 +71,46 @@ class TasksTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
-        cell.textLabel?.text = tasksArray[indexPath.row]
+        cell.textLabel?.text = tasksArray[indexPath.row].task
         return cell
     }
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { (_,_,complition) in
-            self.tasksArray.remove(at: indexPath.row)
+        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") {(_,_,complition) in
+            try! self.realm.write {
+                self.realm.delete(self.tasksArray[indexPath.row])
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
         deleteAction.backgroundColor = .red
         let deleteActionConfig = UISwipeActionsConfiguration(actions: [deleteAction])
         return deleteActionConfig
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var tempArray = [String]()
+        for task in tasksArray {
+            tempArray.append(task.task)
+        }
+        try! realm.write({
+            realm.deleteAll()
+        })
+        let tempItem = tempArray[sourceIndexPath.row]
+        tempArray.remove(at: sourceIndexPath.row)
+        tempArray.insert(tempItem, at: destinationIndexPath.row)
+        for item in tempArray {
+            let task = TaskStruct()
+            task.task = item
+            try! self.realm.write {
+                self.realm.add(task)
+            }
+        }
+        
+    }
+    
+    @IBAction func editButton(_ sender: UIBarButtonItem) {
+        tableView.isEditing = !tableView.isEditing
+        sender.title = tableView.isEditing ? "Закончить" : "Изменить"
     }
     
 }
